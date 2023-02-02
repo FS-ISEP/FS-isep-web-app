41c1bc617c255c3908230872be261bec137d3985
1190329 <1190329@isep.ipp.pt>
2023-02-02 18:35:46 +0000

    Added general styles and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SponsorsPageComponent } from './sponsors-page/sponsors-page.component';
+import { TeamPageComponent } from './team-page/team-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent},
+  { path: 'about', component: AboutPageComponent},
+  { path: 'team', component: TeamPageComponent},
+  { path: 'sponsors', component: SponsorsPageComponent},
+  { path: 'contacts', component: ContactsPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { TeamPageComponent } from './team-page/team-page.component';
+import { SponsorsPageComponent } from './sponsors-page/sponsors-page.component';
+import { ContactsPageComponent } from './contacts-page/contacts-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AboutPageComponent,
+    TeamPageComponent,
+    SponsorsPageComponent,
+    ContactsPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
