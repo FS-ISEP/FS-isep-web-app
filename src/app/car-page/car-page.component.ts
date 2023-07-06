9d03f680482ea374b49965ac183bcc3ca365569a
Guilherme Silveira <1221435@isep.ipp.pt>
2023-07-06 18:26:02 +0100

    Car Page and Car Tarkbar Button

---
+++ src/app/car-page/car-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-car-page',
+  templateUrl: './car-page.component.html',
+  styleUrls: ['./car-page.component.css']
+})
+export class CarPageComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Carro - Formula Student ISEP")
+  }
+  
+}
+
