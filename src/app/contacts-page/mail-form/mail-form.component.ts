05c762ca3e383c55a194506eab79e00a2f5f0933
Guilherme Silveira <133682570+Silveira-Guilherme@users.noreply.github.com>
2023-10-29 16:23:35 +0000

    Features contacts updates (#12)
    * Contacts Update
    
    * Contacts Updates
    
    * Fix pointed issues
    
    * Map Refinement
    
    * Map refinement
    
    * Contacts info refinmenet
    
    * Mail form refinement
    
    * Features socials (#14)
    
    * Socials Updates
    
    Foi pedido na reunião de terça feira, dia 17/10, que o botão do youtube fosse removido da navbar e da página dos contactos
    
    * Adjusted socials width
    
    ---------
    
    Co-authored-by: correiaafonso <119017279+correiaafonso@users.noreply.github.com>
    Co-authored-by: Afonso Correia <afonsomfcorreia@gmail.com>
    
    ---------
    
    Co-authored-by: Afonso Correia <afonsomfcorreia@gmail.com>
    Co-authored-by: correiaafonso <119017279+correiaafonso@users.noreply.github.com>

---
+++ src/app/contacts-page/mail-form/mail-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-form',
+  templateUrl: './mail-form.component.html',
+  styleUrls: ['./mail-form.component.css']
+})
+export class MailFormComponent {
+
+  onSubmit() {
+    console.log("WIP")
+  }
+}
