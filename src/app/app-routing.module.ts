881aa7bffd2ba2532d29a4291fb4e1be2f84356f
Silveira-Guilherme <>
2024-05-07 15:00:31 +0100

    Car, Sponsors, Contacts and Navbar Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,12 @@ import { AboutPageComponent } from './about-page/about-page.component';
 import { ContactsPageComponent } from './contacts-page/contacts-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SponsorsPageComponent } from './sponsors-page/sponsors-page.component';
-import { TeamPageComponent } from './team-page/team-page.component';
-import { ShopPageComponent } from './shop-page/shop-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
   { path: 'about', component: AboutPageComponent},
-  { path: 'team', component: TeamPageComponent},
   { path: 'sponsors', component: SponsorsPageComponent},
-  { path: 'shop', component: ShopPageComponent},
   { path: 'contacts', component: ContactsPageComponent},
   { path: 'car', component: CarPageComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
-import { TeamPageComponent } from './team-page/team-page.component';
 import { SponsorsPageComponent } from './sponsors-page/sponsors-page.component';
 import { ContactsPageComponent } from './contacts-page/contacts-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -13,7 +11,6 @@ import { NavButtonsComponent } from './navbar/nav-buttons/nav-buttons.component'
 import { FooterComponent } from './footer/footer.component';
 import { LangFlagComponent } from './navbar/lang-flag/lang-flag.component';
 import { SocialsComponent } from './navbar/socials/socials.component';
-import { ShopPageComponent } from './shop-page/shop-page.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LangFlagSidebarComponent } from './sidebar/lang-flag-sidebar/lang-flag-sidebar.component';
 import { NavButtonsSidebarComponent } from './sidebar/nav-buttons-sidebar/nav-buttons-sidebar.component';
@@ -28,7 +25,6 @@ import { MapComponent } from './contacts-page/map/map.component';
     AppComponent,
     LandingPageComponent,
     AboutPageComponent,
-    TeamPageComponent,
     SponsorsPageComponent,
     ContactsPageComponent,
     NavbarComponent,
@@ -36,7 +32,6 @@ import { MapComponent } from './contacts-page/map/map.component';
     FooterComponent,
     LangFlagComponent,
     SocialsComponent,
-    ShopPageComponent,
     SidebarComponent,
     LangFlagSidebarComponent,
     NavButtonsSidebarComponent,
