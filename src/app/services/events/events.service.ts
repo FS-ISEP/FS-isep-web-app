1018c8fdf9b7d2070cb8e30bb8156a323a1bb4e4
correiaafonso <119017279+correiaafonso@users.noreply.github.com>
2024-07-20 17:03:35 +0100

    About Page (#22)
    * Initial About Page
    
    * Cherry pick
    
    * Completed about page
    
    * Separated into data seeds
    
    * Moved code blocks to ts
    
    ---------
    
    Co-authored-by: Afonso Correia <1190329@isep.ipp.pt>

---
+++ src/app/services/events/events.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Event } from 'src/app/models/event.model';
+
+export var STATIC_EVENTS: Event[] = [
+  {
+    title: 'ENGINEERING DESIGN',
+    description: 'Os estudantes devem apresentar os seus conhecimentos sobre o carro e conceitos de engenharia a um painel de juízes. Na maioria dos eventos há múltiplas rondas de design, cada uma com perguntas cada vez mais difíceis e sessões mais longas. A primeira ronda envolve todas as equipas, e centra-se nos conhecimentos fundamentais. Apenas as melhores equipas chegam às finais.',
+    imageUrl: './assets/car_images/front.png'
+  },
+  {
+    title: 'ENGINEERING DESIGN',
+    description: 'Os estudantes devem apresentar os seus conhecimentos sobre o carro e conceitos de engenharia a um painel de juízes. Na maioria dos eventos há múltiplas rondas de design, cada uma com perguntas cada vez mais difíceis e sessões mais longas. A primeira ronda envolve todas as equipas, e centra-se nos conhecimentos fundamentais. Apenas as melhores equipas chegam às finais.',
+    imageUrl: './assets/car_images/front.png'
+  },
+  {
+    title: 'ENGINEERING DESIGN',
+    description: 'Os estudantes devem apresentar os seus conhecimentos sobre o carro e conceitos de engenharia a um painel de juízes. Na maioria dos eventos há múltiplas rondas de design, cada uma com perguntas cada vez mais difíceis e sessões mais longas. A primeira ronda envolve todas as equipas, e centra-se nos conhecimentos fundamentais. Apenas as melhores equipas chegam às finais.',
+    imageUrl: './assets/car_images/front.png'
+  }
+]
+
+export var DYNAMIC_EVENTS: Event[] = [
+  {
+    title: 'ENGINEERING DESIGN',
+    description: 'Os estudantes devem apresentar os seus conhecimentos sobre o carro e conceitos de engenharia a um painel de juízes. Na maioria dos eventos há múltiplas rondas de design, cada uma com perguntas cada vez mais difíceis e sessões mais longas. A primeira ronda envolve todas as equipas, e centra-se nos conhecimentos fundamentais. Apenas as melhores equipas chegam às finais.',
+    imageUrl: './assets/car_images/front.png'
+  },
+  {
+    title: 'ENGINEERING DESIGN',
+    description: 'Os estudantes devem apresentar os seus conhecimentos sobre o carro e conceitos de engenharia a um painel de juízes. Na maioria dos eventos há múltiplas rondas de design, cada uma com perguntas cada vez mais difíceis e sessões mais longas. A primeira ronda envolve todas as equipas, e centra-se nos conhecimentos fundamentais. Apenas as melhores equipas chegam às finais.',
+    imageUrl: './assets/car_images/front.png'
+  },
+  {
+    title: 'ENGINEERING DESIGN',
+    description: 'Os estudantes devem apresentar os seus conhecimentos sobre o carro e conceitos de engenharia a um painel de juízes. Na maioria dos eventos há múltiplas rondas de design, cada uma com perguntas cada vez mais difíceis e sessões mais longas. A primeira ronda envolve todas as equipas, e centra-se nos conhecimentos fundamentais. Apenas as melhores equipas chegam às finais.',
+    imageUrl: './assets/car_images/front.png'
+  }
+]
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor() { }
+}
