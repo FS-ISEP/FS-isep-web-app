e50b2256d0ab99bffa22d4e2a16e8858367c06a8
Afonso Correia <1190329@isep.ipp.pt>
2024-07-20 17:18:50 +0100

    Comment old components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { CarPageComponent } from './car-page/car-page.component';
 import { ContactsPageComponent } from './contacts-page/contacts-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SponsorsPageComponent } from './sponsors-page/sponsors-page.component';
-import { TeamPageComponent } from './team-page/team-page.component';
-import { ShopPageComponent } from './shop-page/shop-page.component';
-import { CarPageComponent } from './car-page/car-page.component';
+// import { TeamPageComponent } from './team-page/team-page.component';
+// import { ShopPageComponent } from './shop-page/shop-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
   { path: 'about', component: AboutPageComponent},
-  { path: 'team', component: TeamPageComponent},
-  { path: 'sponsors', component: SponsorsPageComponent},
-  { path: 'shop', component: ShopPageComponent},
-  { path: 'contacts', component: ContactsPageComponent},
   { path: 'car', component: CarPageComponent},
+  { path: 'sponsors', component: SponsorsPageComponent},
+  { path: 'contacts', component: ContactsPageComponent},
+  // { path: 'team', component: TeamPageComponent},
+  // { path: 'shop', component: ShopPageComponent},
 ];
 
 @NgModule({
