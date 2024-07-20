042599ec990fdca952701a688bd0f9bbb00cb40c
Afonso Correia <1190329@isep.ipp.pt>
2024-07-20 16:00:26 +0100

    Completed about page

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+enum IntroTextEnum {
+  HISTORIA,
+  MISSAO
+}
+
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.css']
+  styleUrls: ['./about-page.component.css'],
 })
 export class AboutPageComponent {
+  IntroText = IntroTextEnum
 
-  selectedButton: string = 'Historia'; 
-  selectedColor: string = 'white'; // Selected color (e.g., green)
-  unselectedColor: string = 'rgb(196, 196, 196)'; // Unselected color (e.g., red)
-
+  selectedButton: IntroTextEnum = IntroTextEnum.HISTORIA;
+  selectedColor: string = 'white';
+  unselectedColor: string = 'rgb(196, 196, 196)';
 
   constructor(private titleService: Title) {
     this.titleService.setTitle("Sobre - Formula Student ISEP")
   }
-  selectButton(button: string) {
-      this.selectedButton = button; // Select the clicked button
-    
+
+  selectButton(selected: IntroTextEnum) {
+    this.selectedButton = selected;
   }
 }
-
-
